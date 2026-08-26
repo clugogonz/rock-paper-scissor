2ab8c74b006b7d8750c7e92dcda15801600b6f0a
clugogonz <217103434+clugogonz@users.noreply.github.com>
2026-08-26 11:34:27 -0400

    Add the ability for human player to choose rock, paper or scissors feature normalizes text so that the same choice can be written in any way and still be correct so long as there are no typos.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,23 @@
 //Get the computers choice of rock, paper or scissors//
 function getComputerChoice(){
-    let randomInt = Math.floor(Math.random()*3) + 1;
+    let randomInt = Math.floor(Math.random()*3);
 
-    let choice = (randomInt == 1) ? 'Rock' :
-    (randomInt == 2) ? 'Paper' : 'Scissors';
+    let choice = (randomInt == 0) ? 'Rock' :
+    (randomInt == 1) ? 'Paper' : 'Scissors';
 
     return choice;
+}
+
+//Get the user's choice of rock, paper or scissors
+function getHumanChoice(){
+let userInput = prompt ("Rock, Paper or Scissors?", '');
+
+    let firstLetter = userInput.charAt(0);
+    let capitalized = firstLetter.toUpperCase();
+    let stringBody = userInput.substring(1);
+    let stringNormal= stringBody.toLowerCase();
+    let newString = capitalized+stringNormal
+    return newString;
+
 }
 
